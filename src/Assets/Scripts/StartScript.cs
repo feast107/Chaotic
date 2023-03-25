ecfa29c16a843d6b99ec7b521d58fe91ea9038a9
Feast <1353216996@qq.com>
2023-03-25 10:32:18 +0800

    1

---
+++ src/Assets/Scripts/StartScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
